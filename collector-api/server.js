c25be9527cbe6dc1c404109b6391b1ea31cafd21
jlangston <jlangston@pnimedia.com>
2017-06-20 13:53:12 -0700

    Added mongo db to node server

--- collector-api/server.js
+++ collector-api/server.js
@@ -1,7 +1,23 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3001;
+  port = process.env.PORT || 3001,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/collectorModel'),
+  bodyParser = require('body-parser');
+  
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/collectorDB'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/collectorRoutes');
+routes(app);
+
 
 app.listen(port);
+
 
 console.log('todo list RESTful API server started on: ' + port);
