2bbc101e196f959cf0e87a1161ad6b7fb01237fa
Jeffrey Langston <jeffl@orbitaltech.com>
2018-01-22 20:35:35 -0800

    webpack install

---
+++ collector-api/README.md
@@ -0,0 +1,1 @@
+Ensure mongod is running.
--- collector-web/src/components/main/main-component.js
+++ collector-web/src/components/main/main-component.js
@@ -14,106 +14,104 @@ import gridCell from '../grid/grid-cell';
 
 injectTapEventPlugin();
 
-export default class MainComponent extends React.Component{
-
-    constructor(props){
-        super(props);
-        this.state = 
+export default class MainComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state =
         {
-            sliderMenuOpen:false,
-            gridColumns: [
-                new gridColumn('Title'),
-                new gridColumn('Author'),
-                new gridColumn('Own'),
-                new gridColumn('Rating'),
-                new gridColumn('Want')
-            ],
-            gridData: [
-             new gridRow(
-                1, [
-                    new gridCell('Title', 'book1'),
-                    new gridCell('Author', 'author1'),
-                    new gridCell('Own', 'False'),
-                    new gridCell('Rating', '1'),
-                    new gridCell('Want', 'True')
-                ]
-                ),
+          sliderMenuOpen: false,
+          gridColumns: [
+            new gridColumn('Title'),
+            new gridColumn('Author'),
+            new gridColumn('Own'),
+            new gridColumn('Rating'),
+            new gridColumn('Want'),
+          ],
+          gridData: [
             new gridRow(
-                2, [
-                    new gridCell('Title', 'book2'),
-                    new gridCell('Author', 'author2'),
-                    new gridCell('Own', 'False'),
-                    new gridCell('Rating', '2'),
-                    new gridCell('Want', 'True')
-                ]
-                ) 
-            ]
+              1, [
+                new gridCell('Title', 'book1'),
+                new gridCell('Author', 'author1'),
+                new gridCell('Own', 'False'),
+                new gridCell('Rating', '1'),
+                new gridCell('Want', 'True'),
+              ],
+            ),
+            new gridRow(
+              2, [
+                new gridCell('Title', 'book2'),
+                new gridCell('Author', 'author2'),
+                new gridCell('Own', 'False'),
+                new gridCell('Rating', '2'),
+                new gridCell('Want', 'True'),
+              ],
+            ),
+          ],
         };
 
-        this.handleToggle = this.handleToggle.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-        this.setMenuState = this.setMenuState.bind(this);
-        this.addItem = this.addItem.bind(this);
-        this.deleteItem = this.deleteItem.bind(this);
-        this.saveItem = this.saveItem.bind(this);
-        this.undoChanges = this.undoChanges.bind(this);
-        this.handleCellChange = this.handleCellChange.bind(this);
-        
-    }
-   
-    handleCellChange = (event) => {
-        this.setState((previousState) => {
-            console.log(event);
-            //           previousState.xyz = 'blurg';
-           return previousState;
-        });
-    }
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.setMenuState = this.setMenuState.bind(this);
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.saveItem = this.saveItem.bind(this);
+    this.undoChanges = this.undoChanges.bind(this);
+    this.handleCellChange = this.handleCellChange.bind(this);
+  }
 
-    addItem(){
-        
-    }
+  handleCellChange(event) {
+    this.setState((previousState) => {
+      console.log(event);
+      //           previousState.xyz = 'blurg';
+      return previousState;
+    });
+  }
 
-    deleteItem(){
+  addItem() {
 
-    }
+  }
 
-    saveItem(){
+  deleteItem() {
 
-    }
+  }
 
-    undoChanges(){
+  saveItem() {
 
-    }
+  }
 
-    handleToggle(){
-        this.setState({
-            sliderMenuOpen: !this.state.sliderMenuOpen
-        });
-    }
+  undoChanges() {
 
-     setMenuState(state){
-        this.setState({
-            sliderMenuOpen: state
-        });
-    }
+  }
 
-    handleClose(){
-        this.setState({
-            sliderMenuOpen: false
-        });
-    }
-  
-    render() {
+  handleToggle() {
+    this.setState({
+      sliderMenuOpen: !this.state.sliderMenuOpen,
+    });
+  }
+
+  setMenuState(state) {
+    this.setState({
+      sliderMenuOpen: state,
+    });
+  }
+
+  handleClose() {
+    this.setState({
+      sliderMenuOpen: false,
+    });
+  }
+
+  render() {
     const isSliderMenuOpen = this.state.sliderMenuOpen;
- 
-        return (
-            <div>
-                <SlideMenu menuOpen={isSliderMenuOpen} onMenuClose={this.handleClose} setMenuState={this.setMenuState} />
-                <MainAppBar openSliderMenu={this.handleToggle}/>
-                <Toolbar />
-                <Grid gridColumns={this.state.gridColumns} gridData={this.state.gridData} handleCellChange={this.handleChange}/>
+
+    return (
+          <div>
+              <SlideMenu menuOpen={isSliderMenuOpen} onMenuClose={this.handleClose} setMenuState={this.setMenuState} />
+              <MainAppBar openSliderMenu={this.handleToggle} />
+              <Toolbar />
+              <Grid gridColumns={this.state.gridColumns} gridData={this.state.gridData} handleCellChange={this.handleChange} />
             </div>
-        )
-    }
+    );
+  }
 }
 
